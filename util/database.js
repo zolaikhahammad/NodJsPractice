f9f21e05de0fa005f71a09a23d46ef08a5824b54
Zolaikha Hammad <70544823+zolaikhahammad@users.noreply.github.com>
2022-08-29 02:23:27 +0500

    Connecting our app to local mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,20 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+// const sequelize = new Sequelize('node-complete', 'root', '', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodbConnect=(callback)=>{
+  const mongodb=require('mongodb');
+  const MongoDbClient=mongodb.MongoClient;
+  MongoDbClient.connect('mongodb://127.0.0.1:27017').then(result=>{console.log('connected')
+callback(result);
+}).catch(err=>{console.log(err)});
+  
+}
+module.exports=mongodbConnect;
+
+
